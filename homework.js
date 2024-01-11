2c932de393e0316f8fadedb9d5cd0606c19d1996
JaloliddinKulmamatov <itjalol2010@gmail.com>
2024-01-12 01:59:54 +0500

    jalol

---
+++ homework.js
@@ -0,0 +1,32 @@
+//1
+
+// function Trm (str) {
+// for (let i = 0; i < str.length; i++) {
+//     if (str[i] == ' ') {
+//         this.str = str.replace(' ','');
+//     }  
+// }return str
+// }
+// res = new Trm("  Hello World!    ");
+
+// console.log(res);
+
+//3
+
+// let str = "123 salom";
+
+// for ( syb of str) {
+// }
+
+// function Has(str, i, el) {
+//     if (str[syb] === el) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// let res = Has(str, '3');
+// console.log(res); 
+
+//4
